943f4196729907e9af717fd162ab274bcea4ece3
Sayam Shah <shayamshah1999@gmail.com>
2024-09-14 11:14:45 +0100

    This is the commit from windows server

---
+++ src/main/java/com/democompany/this_is_demo/App.java
@@ -0,0 +1,10 @@
+package com.democompany.this_is_demo;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
